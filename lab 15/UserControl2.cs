322771476d7f46eae9288bb1fddddd2ce67f23a5
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-01-14 14:05:35 +0200

    Full done program

--- lab 15/UserControl2.cs
+++ lab 15/UserControl2.cs
@@ -14,7 +14,7 @@ namespace lab_15
 {
     public partial class UserControl_String : UserControl
     {
-        internal string path = @"D:\visual studio проекти\2 курс\2 семестер\ООП\lab 15 – копія\file.txt";
+        internal string path = @"D:\visual_studio_project\2_course\2_term\OOP\lab_15\File1.txt";
         public UserControl_String()
         {
             InitializeComponent();
